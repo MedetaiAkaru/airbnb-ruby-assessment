f39b8feb96f6d0b46e2d8a84260251bb6c71254f
MedetaiAkaru <akaru@littlix.com>
2016-01-07 11:14:46 +0800

    Basic booking system done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,11 @@
+#booking
+get '/properties/:id/book' do
+  @property = Property.find(params[:id])
+  erb :"/properties/book"
+end
+
+#booking (db)
+post '/properties/:id/book' do
+  booking = Booking.create(date_start: params[:datestart], date_end: params[:dateend], user_id: session[:user_id], property_id: params[:id])
+  redirect "/properties/#{params[:id]}"
+end
